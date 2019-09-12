c3cfaa4ffe50a9646e44f60b45ac4839bb6b2227
eugene <genya12321@gmail.com>
2019-09-12 14:29:00 +0300

    Add JWT support for points

--- src/main/java/com/rakuten/miniichiba/payments/paypal/PayPalPaymentMethod.kt
+++ src/main/java/com/rakuten/miniichiba/payments/paypal/PayPalPaymentMethod.kt
@@ -21,9 +21,18 @@ import java.io.Serializable
 import java.net.URI
 import khttp.post
 import javax.servlet.http.HttpServletResponse
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import khttp.structures.authorization.Authorization
+import org.json.JSONObject
+import java.util.*
+import java.util.Calendar
+
+
+
 
 data class PayPalPaymentData(val id: String, val token: String, val successUri: URI, val cancelUri: URI,
-                             val pointsTransactionId: String?): Serializable
+                             val pointsTransactionId: String?, val headers: Map<String, String?>): Serializable
 
 interface PayPalPaymentStorage {
     fun registerPayment(data: PayPalPaymentData)
@@ -46,7 +55,7 @@ class PayPalBeanFactory {
 class PayPalPaymentController @Autowired constructor(
         private val storage: PayPalPaymentStorage,
         private val context: APIContext,
-        @Value("\${points.url}") internal val pointsUrl: String) {
+        @JacksonInject val jsonContext: PayPalJsonContext) {
 
     @RequestMapping(ACCEPT_REQUEST)
     fun processAcceptedPayment(
@@ -64,14 +73,16 @@ class PayPalPaymentController @Autowired constructor(
             payment.execute(context, execution)
             response.setHeader("Location", paymentData.successUri.toString())
             if (paymentData.pointsTransactionId != null) {
-                val transactionPayload = mapOf("transactionId" to paymentData.pointsTransactionId)
-                post(url = "$pointsUrl/points/transaction/confirm", data = transactionPayload)
+                val transactionPayload = JSONObject(mapOf("id" to paymentData.pointsTransactionId))
+                post(url = "${jsonContext.pointsUrl}/points/transaction/confirm", data = transactionPayload,
+                        headers = paymentData.headers)
             }
         } catch (e: Throwable) {
             response.setHeader("Location", paymentData.cancelUri.toString())
             if (paymentData.pointsTransactionId != null) {
-                val transactionPayload = mapOf("transactionId" to paymentData.pointsTransactionId)
-                post(url = "$pointsUrl/points/transaction/cancel", data = transactionPayload)
+                val transactionPayload = JSONObject(mapOf("transactionId" to paymentData.pointsTransactionId))
+                post(url = "${jsonContext.pointsUrl}/points/transaction/cancel", data = transactionPayload,
+                        headers = paymentData.headers)
             }
             // TODO Log the error
         }
@@ -81,8 +92,9 @@ class PayPalPaymentController @Autowired constructor(
     fun processCancelledPayment(response: HttpServletResponse, @RequestParam token: String) {
         val paymentData = storage.getAndRemovePaymentWithToken(token) ?: return
         if (paymentData.pointsTransactionId != null) {
-            val transactionPayload = mapOf("transactionId" to paymentData.pointsTransactionId)
-            post(url = "$pointsUrl/points/transaction/cancel", data = transactionPayload)
+            val transactionPayload = JSONObject(mapOf("transactionId" to paymentData.pointsTransactionId))
+            post(url = "${jsonContext.pointsUrl}/points/transaction/cancel", data = transactionPayload,
+                    headers = paymentData.headers)
         }
         response.status = HttpServletResponse.SC_FOUND
         response.setHeader("Location", paymentData.cancelUri.toString())
@@ -116,14 +128,17 @@ class PayPalPaymentRequest @JsonCreator constructor(
         var pointsDiscount = pointsString.toBigDecimal()
         val fixedItems = mutableListOf<Item>()
         var totalPrice = BigDecimal.ZERO
+        var pointsSpent = BigDecimal.ZERO
 
         for (item in details.items) {
             val quantity = if (item.quantity != null) item.quantity.toBigDecimal() else BigDecimal.ONE
             val oldPrice = item.price.value.toBigDecimal().multiply(quantity)
             if (oldPrice <= pointsDiscount) {
+                pointsSpent = pointsSpent.plus(oldPrice)
                 pointsDiscount = pointsDiscount.minus(oldPrice)
                 fixedItems.add(Item(item.name, item.quantity ?: "1", "0", item.price.currency))
             } else {
+                pointsSpent = pointsSpent.plus(pointsDiscount)
                 var newPrice = oldPrice.minus(pointsDiscount)
                 pointsDiscount = BigDecimal.ZERO
                 val newItemPrice = newPrice.divide(quantity).setScale(2, RoundingMode.CEILING)
@@ -133,9 +148,30 @@ class PayPalPaymentRequest @JsonCreator constructor(
             }
         }
         var transactionId: String? = null
-        if (pointsDiscount > BigDecimal.ZERO) {
-            val transactionPayload = mapOf("userId" to details.userId, "amount" to pointsString)
-            val r = post(url = "${context.pointsUrl}/points/transaction/start", data = transactionPayload)
+        var headers: Map<String, String?> = Collections.emptyMap()
+        if (pointsSpent > BigDecimal.ZERO) {
+            val transactionPayload = JSONObject(mapOf("expires" to "2019-12-12T08:38:28.502Z",
+                    "amount" to -(pointsSpent.toInt()),
+                    "comment" to "string"))
+            // Base64 encoded "qwertyuiopasdfghjklzxcvbnm123456"
+            val key = "cXdlcnR5dWlvcGFzZGZnaGprbHp4Y3Zibm0xMjM0NTY="
+
+            val currentDate = Date()
+            val c = Calendar.getInstance()
+            c.time = currentDate
+            c.add(Calendar.DATE, 5)
+            val expDate = c.timeInMillis / 1000
+            val jti = UUID.randomUUID()
+            val payload = "{\"userId\":\"" + details.userId + "\",\"jti\":\"" + jti + "\",\"exp\":" +
+                    expDate.toString() + "}"
+            val jwt = Jwts.builder()
+                    .setPayload(payload)
+                    .setHeaderParam("alg", "HS256")
+                    .setHeaderParam("typ", "JWT")
+            val jws = jwt.signWith(SignatureAlgorithm.HS256, key).compact()
+            headers = mapOf("Authorization" to "Bearer $jws", "Content-Type" to "application/json")
+            val r = post(url = "${context.pointsUrl}/points/transaction/start", headers = headers,
+                    data = transactionPayload)
             transactionId = r.jsonObject.get("transactionId").toString()
         }
         if (totalPrice > BigDecimal.ZERO) {
@@ -159,11 +195,11 @@ class PayPalPaymentRequest @JsonCreator constructor(
             val approvalUrl = URI(created.links.find { it.rel == "approval_url" }!!.href)
             val token = approvalUrl.query.split('&').find { it.startsWith("token=") }!!.substring(6)
             context.paymentStorage.registerPayment(PayPalPaymentData(created.id, token, successUri, cancelUri,
-                    transactionId))
+                    transactionId, headers))
             return RedirectPaymentResult(approvalUrl)
         } else {
-            val payload = mapOf("transactionId" to transactionId)
-            post(url="${context.pointsUrl}/points/transaction/confirm", data=payload)
+            val payload = JSONObject(mapOf("id" to transactionId))
+            val r = post(url="${context.pointsUrl}/points/transaction/confirm", data=payload, headers = headers)
             return SuccessPaymentResult()
         }
     }
